7499a6c654f9e9cccf78c91110e0bfeade953e17
Stefan van der Grift <edalbrelord@gmail.com>
2021-01-19 18:23:44 +0100

    Desktop layout

--- pages/index.vue
+++ pages/index.vue
@@ -8,11 +8,17 @@
         <h1 class="text-3xl text-blue-dark-grey font-semibold mb-5">Powered by technology</h1>
         <h3 class="text-lg text-blue-gray">Our Artificial Intelligence powered tools use million of project data points to ensure that your project is successful</h3>
       </div>
-      <div class="flex flex-col space-y-8">
-        <FeatureCard title="Supervisor" description="Monitors activity to identify project roadblocks" image="icon-supervisor" color="cyan"/>
-        <FeatureCard title="Team builder" description="Scans our talent network to create the optimal team for your project" image="icon-team-builder" color="red"/>
-        <FeatureCard title="Karma" description="Regularly evaluates our talent to ensure quality" image="icon-karma" color="orange"/>
-        <FeatureCard title="Calculator" description="Uses data from past projects to provide better delivery estimates" image="icon-calculator" color="blue"/>
+      <div class="flex flex-col space-y-8 md:flex-row md:items-center md:space-y-0 md:space-x-8">
+        <div class="flex flex-col md:w-1/3">
+          <FeatureCard title="Supervisor" description="Monitors activity to identify project roadblocks" image="icon-supervisor" color="cyan"/>
+        </div>
+        <div class="flex flex-col space-y-8 md:w-1/3">
+          <FeatureCard title="Team builder" description="Scans our talent network to create the optimal team for your project" image="icon-team-builder" color="red"/>
+          <FeatureCard title="Karma" description="Regularly evaluates our talent to ensure quality" image="icon-karma" color="orange"/>
+        </div>
+        <div class="flex flex-col md:w-1/3">
+          <FeatureCard title="Calculator" description="Uses data from past projects to provide better delivery estimates" image="icon-calculator" color="blue"/>
+        </div>
       </div>
     </div>
   </div>
